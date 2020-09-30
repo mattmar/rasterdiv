3ee738fc181038ea5e4b9eb71541fcd556a1759d
mattmar <marcantoniomatteo@gmail.com>
2020-09-30 15:43:23 -0700

    Adding progress package to DESCRIPTION

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(names=c("cl","mfactor","clusterExport","%dopar%","makeCluster","registerDoParallel","stopCluster"), add=F)
+utils::globalVariables(names=c("cl","mfactor","clusterExport","%dopar%","makeCluster","registerDoParallel","stopCluster","progress_bar"), add=F)

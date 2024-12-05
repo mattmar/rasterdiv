f653245335ed793c71fe3f0ec96f268470495f8a
mattmar <marcantoniomatteo@gmail.com>
2024-12-05 14:40:32 +0100

    further bug fix on paRao() area

--- R/paRaoAreaS.R
+++ R/paRaoAreaS.R
@@ -31,7 +31,7 @@ paRaoAreaS <- function(rasterm, area, alpha, simplify) {
 	storage.mode(crop1dt) <- "integer"
 
 # Check for only 1 value in the matrix
-if( all(apply(crop1dt, 2, function(a) length(unique(a))<=3)) ) {
+if( all( apply(crop1dt, 2, function(a) length(unique(a[which(!is.na(a))])==1))) ) {
 	paRaoOareaS <- NA
 	} else {
 		classes <- levels(as.factor(crop1dt))
